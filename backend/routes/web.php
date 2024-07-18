06ef04caf25cf8164ecd60370dbde88c12bd0d41
Loick Hommet <hommet.loick@gmail.com>
2024-07-18 18:04:56 +0200

    ajout route login

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,3 +10,4 @@ Route::get('/', function () {
 
 Route::post('/api/register', [RegisterController::class, 'register'])->middleware('api');
 
+Route::post('/api/login', [LoginController::class, 'login'])->middleware('api');
