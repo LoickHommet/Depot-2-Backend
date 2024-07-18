7506f9b95a65ac105efe559e9d87c3ae463e33bd
Loick Hommet <hommet.loick@gmail.com>
2024-07-18 16:12:22 +0200

    Creation de la function register

--- backend/app/Http/Controllers/Auth/RegisterController.php
+++ backend/app/Http/Controllers/Auth/RegisterController.php
@@ -4,8 +4,31 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
 class RegisterController extends Controller
 {
-    //
+    public function register(Request $request){
+       
+        $validator =  Validator::make($request->all(), [
+            'firstname' => 'required|string|min:2',
+            'name' => 'required|string|min:2',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $user = User::create([
+            'firstname' => $request->firstname,
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        return response()->json(['message' => 'User registered successfully', 'user' => $user], 201);
+
+    }
 }
