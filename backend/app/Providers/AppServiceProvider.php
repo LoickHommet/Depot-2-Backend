46151b4d9da7abf41320d97b5d381752a2fdafd0
Loick Hommet <hommet.loick@gmail.com>
2024-07-18 16:11:08 +0200

    VerifyCsrfToken

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -11,7 +11,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::except([
+            'submit'
+        ]);
     }
 
     /**
