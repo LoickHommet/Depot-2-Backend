4d9afaab1173c7ec408876139cb237b0ce041fa1
Loick Hommet <hommet.loick@gmail.com>
2024-07-19 12:07:49 +0200

    Creation de la route api/expenses

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\ExpenseController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -13,3 +14,5 @@ Route::post('/api/register', [RegisterController::class, 'register'])->middlewar
 Route::post('/api/login', [LoginController::class, 'login'])->middleware('api');
 
 Route::post('/api/logout', [LoginController::class, 'logout'])->name('logout');
+
+Route::post('/api/expenses', [ExpenseController::class, 'store'])->middleware('api');
